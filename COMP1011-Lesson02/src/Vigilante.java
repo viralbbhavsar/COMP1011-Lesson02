e2dbcbd44c9a9882fe9c0e775d6d9296e7686929
viralbbhavsar <viralbbhavsar@gmail.com>
2015-01-15 16:23:03 -0500

    added vigilante

---
+++ COMP1011-Lesson02/src/Vigilante.java
@@ -0,0 +1,12 @@
+
+public class Vigilante extends Villian {
+	// constructor---------------------------------------------------
+	public Vigilante(String name) {
+		super(name);
+		
+	}
+
+	public void dealsJustice() {
+		System.out.println(this.name+ "is dealing Justice, baby");
+	}
+}
