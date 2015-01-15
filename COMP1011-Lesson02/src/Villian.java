9d5f6307df7e96e580a97ad707614f67cf71003e
viralbbhavsar <viralbbhavsar@gmail.com>
2015-01-15 16:15:50 -0500

    added villian

---
+++ COMP1011-Lesson02/src/Villian.java
@@ -0,0 +1,15 @@
+
+public class Villian extends Hero {
+
+	public Villian(String name) {
+		super(name);
+		
+	}
+	
+	//public methods----------------------------------------------
+	
+	public void steals() {
+		System.out.println(this.name+ "is stealing");
+	}
+
+}
