c24a6dca47d6237af6cd31f2104452f6868ad802
viralbbhavsar <viralbbhavsar@gmail.com>
2015-01-15 16:03:12 -0500

    new hero

---
+++ COMP1011-Lesson02/src/Project.java
@@ -0,0 +1,15 @@
+
+public class Project {
+
+	public static void main(String[] args) {
+		
+		//Instantiate Hero
+		
+		Hero hero = new Hero("Big Man");
+		hero.fight();
+		hero.run();
+		hero.showAbilities();
+
+	}
+
+}
